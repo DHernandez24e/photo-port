cda8ffcee451d3e75d5c1acf5451dc326559f506
Doggo <dhernandez24e@hotmail.com>
2020-07-24 18:18:26 -0700

    add contact form tests

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('ContactForm component', () => {
+    it('renders', () => {
+        render(<ContactForm/>)
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<ContactForm/>)
+
+        expect(asFragment()).toMatchSnapshot()
+    })
+});
+
+describe('finds content', () => {
+    it('has Contact Me in element', () => {
+        const { getByTestId } = render(<ContactForm/>)
+
+        expect(getByTestId('contact')).toHaveTextContent('Contact me');
+        expect(getByTestId('submit-btn')).toHaveTextContent('Submit');
+    })
+})
